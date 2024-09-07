29a02794e5480c80891910bb6494729bc6899397
Yaroslav Klimkov <yaroslav.klimkov@n-able.com>
2024-09-07 13:07:14 +0200

    fix: use json instead of api

--- .github/scripts/check_branch_name.py
+++ .github/scripts/check_branch_name.py
@@ -1,19 +1,16 @@
-# check_branch_name.py
 import re
 import os
-import requests
+import json
 
 # Function to get branch name
 def get_branch_name():
-    if os.getenv('GITHUB_EVENT_NAME') == 'pull_request':
-        pr_number = os.getenv('GITHUB_REF').split('/')[-1]
-        response = requests.get(f'https://api.github.com/repos/{os.getenv("GITHUB_REPOSITORY")}/pulls/{pr_number}',
-                                headers={'Authorization': f'token {os.getenv("GITHUB_TOKEN")}'})
-        response.raise_for_status()
-        pr_data = response.json()
-        return pr_data['head']['ref']
-    else:
-        return os.getenv('GITHUB_REF').split('/')[-1]
+    # Load the GitHub event data from the event path
+    event_path = os.getenv('GITHUB_EVENT_PATH')
+    with open(event_path) as f:
+        event_data = json.load(f)
+
+    # Get the branch name from the pull request data
+    return event_data['pull_request']['head']['ref']
 
 # Check branch name against pattern
 branch_name = get_branch_name()
